ammarfaizi2/gwhttpd

commit c3fd714c0525227a06c1e5b62a4a1be9fd3d295c

Full project rewrite
Big changes. Not worth bisecting. Let's start over from zero.

Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

gwnet_http.h
@@ -9,10 +9,21 @@
 #define _GNU_SOURCE
 #endif
 
+#include "gwnet_http1.h"
 #include "gwnet_tcp.h"
+
+#include <stdbool.h>
 
 #define GWNET_HTTP_DEF_MAX_REQ_HDR_LEN	8192
 #define GWNET_HTTP_DEF_MAX_REQ_BODY_LEN	(1024 * 1024 * 10)
+
+enum {
+	GWNET_HTTP_RES_TYPE_NO_CONTENT	= 0,
+	GWNET_HTTP_RES_TYPE_BUF		= 1,
+	GWNET_HTTP_RES_TYPE_ZERO	= 2,
+	GWNET_HTTP_RES_TYPE_URANDOM	= 3,
+	GWNET_HTTP_RES_TYPE_FILE	= 4,
+};
 
 struct gwnet_http_srv_cfg {
 	struct gwnet_tcp_srv_cfg	tcp_cfg;
@@ -20,34 +31,66 @@ struct gwnet_http_srv_cfg {
 	uint64_t			max_req_body_len;
 };
 
-struct gwnet_http_hdr_pair {
-	char	*key;
-	char	*val;
-};
-
-struct gwnet_http_hdr {
-	struct gwnet_http_hdr_pair	*pairs;
-	size_t				nr_pairs;
-};
-
 struct gwnet_http_srv;
 struct gwnet_http_cli;
 struct gwnet_http_req;
+struct gwnet_http_res;
 
 typedef struct gwnet_http_srv gwnet_http_srv_t;
-typedef int (*gwnet_http_srv_route_cb_t)(struct gwnet_http_srv *srv,
-					 struct gwnet_http_cli *hc,
-					 struct gwnet_http_req *req);
+typedef struct gwnet_http_cli gwnet_http_cli_t;
+typedef struct gwnet_http_req gwnet_http_req_t;
+typedef struct gwnet_http_res gwnet_http_res_t;
 
-const char *gwnet_http_hdr_get_val(struct gwnet_http_hdr *hdr, const char *key);
-int gwnet_http_hdr_addf(struct gwnet_http_hdr *hdr, const char *key,
-			const char *fmt, ...);
-int gwnet_http_hdr_add(struct gwnet_http_hdr *hdr, const char *key,
-		       const char *val);
+typedef int (*gwnet_http_srv_accept_cb_t)(void *data, gwnet_http_srv_t *s,
+					  gwnet_http_cli_t *c);
+
+typedef int (*gwnet_http_srv_rt_cb_t)(void *data, gwnet_http_srv_t *s,
+					 gwnet_http_cli_t *c,
+					 gwnet_http_req_t *req);
+
+typedef int (*gwnet_http_srv_rt_on_hdr_cb_t)(void *data, gwnet_http_srv_t *s,
+					     gwnet_http_cli_t *c,
+					     gwnet_http_req_t *req);
+
+typedef int (*gwnet_http_srv_rt_on_body_cb_t)(void *data, gwnet_http_srv_t *s,
+					      gwnet_http_cli_t *c,
+					      gwnet_http_req_t *req);
+
 gwnet_http_srv_t *gwnet_http_srv_init(const struct gwnet_http_srv_cfg *cfg);
-int gwnet_http_srv_run(gwnet_http_srv_t *s);
-void gwnet_http_srv_free(struct gwnet_http_srv *s);
-void gwnet_http_srv_set_route_cb(gwnet_http_srv_t *s,
-				 gwnet_http_srv_route_cb_t cb, void *data);
+int gwnet_http_srv_run(gwnet_http_srv_t *srv);
+void gwnet_http_srv_stop(gwnet_http_srv_t *srv);
+void gwnet_http_srv_free(gwnet_http_srv_t *srv);
+
+void gwnet_http_srv_set_data_cb(gwnet_http_srv_t *srv, void *data);
+void gwnet_http_srv_set_accept_cb(gwnet_http_srv_t *srv,
+				  gwnet_http_srv_accept_cb_t cb);
+void gwnet_http_cli_set_data_cb(gwnet_http_cli_t *hc, void *data);
+void gwnet_http_cli_set_rt_cb(gwnet_http_cli_t *hc,
+			      gwnet_http_srv_rt_cb_t cb);
+void gwnet_http_cli_set_rt_on_hdr_cb(gwnet_http_cli_t *hc,
+				     gwnet_http_srv_rt_on_hdr_cb_t cb);
+void gwnet_http_cli_set_rt_on_body_cb(gwnet_http_cli_t *hc,
+				      gwnet_http_srv_rt_on_body_cb_t cb);
+
+
+struct gwnet_http_res *gwnet_http_req_get_res(gwnet_http_req_t *req);
+
+void gwnet_http_res_set_code(gwnet_http_res_t *res, uint8_t code);
+void gwnet_http_res_set_content_type(gwnet_http_res_t *res,
+				     const char *content_type);
+void gwnet_http_res_body_set_zero(gwnet_http_res_t *res, uint64_t len);
+void gwnet_http_res_body_set_urandom(gwnet_http_res_t *res, uint64_t len);
+void gwnet_http_res_body_set_file(gwnet_http_res_t *res, int fd,
+				  uint64_t len);
+int gwnet_http_res_body_set_file_path(gwnet_http_res_t *res,
+				      const char *path);
+void gwnet_http_res_body_set_buf(gwnet_http_res_t *res, struct gwbuf *buf);
+struct gwbuf *gwnet_http_res_body_get_buf(gwnet_http_res_t *res);
+
+struct gwnet_http_hdr_fields *gwnet_http_res_get_hdr_fields(
+						gwnet_http_res_t *res);
+
+struct gwnet_http_req_hdr_fields *gwnet_http_req_get_hdr_fields(
+						gwnet_http_req_t *req);
 
 #endif /* #ifndef GWNET_HTTP_H */

.gitignore
@@ -1,4 +1,4 @@
-/gwhttpd2
+/gwhttpd
 *.o
 *.o.d
 /.vscode/

Makefile
@@ -1,18 +1,41 @@
-CFLAGS = -Wall -Wextra -Os -g -fno-strict-aliasing -ffunction-sections -fdata-sections -fvisibility=hidden -std=gnu99 -fno-stack-protector -flto
-LDFLAGS = -flto
+CFLAGS = -Wall -Wextra -Os -g -fno-strict-aliasing -ffunction-sections \
+	-fdata-sections -fvisibility=hidden -std=gnu99 -fno-stack-protector
+LDFLAGS =
 DEPFLAGS = -MMD -MP -MF $@.d
 LIBS = -lpthread
+
+ifeq ($(SANITIZE),1)
+	CFLAGS += -fsanitize=address -fsanitize=undefined
+	LDFLAGS += -fsanitize=address -fsanitize=undefined
+	LIBS += -static-libasan
+endif
+
+ifeq ($(LTO),1)
+	CFLAGS += -flto
+	LDFLAGS += -flto
+endif
+
+ifeq ($(STATIC),1)
+	LDFLAGS += -static
+endif
+
+ifeq ($(DEBUG),1)
+	CFLAGS += -DDEBUG
+else
+	CFLAGS += -DNDEBUG
+endif
 
 GWHTTPD_CC_OBJ = \
 	gwbuf.o \
 	gwnet_http.o \
+	gwnet_http1.o \
 	gwnet_tcp.o \
-	gwhttpd2.o
+	gwhttpd.o
 
-all: gwhttpd2
+all: gwhttpd
 
-gwhttpd2: $(GWHTTPD_CC_OBJ)
-	$(CC) $(CFLAGS) -o $@ $^ $(LDFLAGS) $(LIBS)
+gwhttpd: $(GWHTTPD_CC_OBJ)
+	$(CC) $(CFLAGS) -o $@ $^ $(LIBS) $(LDFLAGS)
 
 %.o: %.c
 	$(CC) $(CFLAGS) $(DEPFLAGS) -c $< -o $@
@@ -20,6 +43,6 @@ gwhttpd2: $(GWHTTPD_CC_OBJ)
 -include $(GWHTTPD_CC_OBJ:.o=.o.d)
 
 clean:
-	rm -f *.o *.o.d gwhttpd2
+	rm -f *.o *.o.d gwhttpd
 
 .PHONY: all clean

common.h
@@ -1,0 +1,30 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (C) 2025  Ammar Faizi <ammarfaizi2@gnuweeb.org>
+ */
+#ifndef COMMON_H
+#define COMMON_H
+
+#ifndef fallthrough
+#define fallthrough __attribute__((__fallthrough__))
+#endif
+
+#ifndef likely
+#define likely(x) __builtin_expect(!!(x), 1)
+#endif
+
+#ifndef unlikely
+#define unlikely(x) __builtin_expect(!!(x), 0)
+#endif
+
+#ifndef __cold
+#define __cold __attribute__((__cold__))
+#endif
+
+#ifndef __hot
+#define __hot __attribute__((__hot__))
+#endif
+
+#include "syscall.h"
+
+#endif /* #ifndef COMMON_H */

gwbuf.c
@@ -1,93 +1,121 @@
 
 #include "gwbuf.h"
+#include "common.h"
 
 #include <stdlib.h>
 #include <string.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <errno.h>
+#include <assert.h>
 
-int gwbuf_init(struct gwbuf *b, size_t cap)
+int gwbuf_init(struct gwbuf *b, uint64_t cap)
 {
-	if (cap == 0)
-		cap = 1023;
+	if (!cap)
+		cap = 1023ull;
 
-	b->buf = malloc(cap + 1);
+	b->orig_buf = b->buf = malloc(cap + 1ull);
 	if (!b->buf)
 		return -ENOMEM;
 
+	b->buf[cap] = '\0';
 	b->len = 0;
 	b->cap = cap;
-	b->buf[0] = b->buf[cap] = '\0';
 	return 0;
 }
 
-int gwbuf_increase(struct gwbuf *b, size_t inc)
+static int gwbuf_set_cap(struct gwbuf *b, uint64_t new_cap)
 {
-	size_t new_cap = b->cap + inc;
 	char *new_buf;
 
-	if (new_cap < b->cap)
-		return -ENOMEM; // Overflow
-
-	new_buf = realloc(b->buf, new_cap + 1);
+	new_buf = realloc(b->buf, new_cap + 1ull);
 	if (!new_buf)
 		return -ENOMEM;
 
-	b->buf = new_buf;
+	b->orig_buf = b->buf = new_buf;
 	b->cap = new_cap;
-	b->buf[b->len] = b->buf[b->cap] = '\0';
+	b->len = (b->len > new_cap) ? new_cap : b->len;
+	b->buf[b->cap] = b->buf[b->len] = '\0';
 	return 0;
 }
 
-void gwbuf_free(struct gwbuf *b)
+__hot
+int gwbuf_increase(struct gwbuf *b, uint64_t inc)
 {
-	if (b->buf) {
-		free(b->buf);
-		b->buf = NULL;
-		b->len = 0;
-		b->cap = 0;
-	}
+	if (!inc)
+		return 0;
+
+	return gwbuf_set_cap(b, b->cap + inc);
 }
 
-void gwbuf_advance(struct gwbuf *b, size_t len)
+__hot
+void gwbuf_free(struct gwbuf *b)
 {
-	if (len > b->len)
+	if (!b || !b->buf || !b->orig_buf)
 		return;
 
-	if (len == b->len) {
+	free(b->orig_buf);
+	memset(b, 0, sizeof(*b));
+}
+
+__hot
+void gwbuf_advance(struct gwbuf *b, uint64_t len)
+{
+	if (len >= b->len) {
 		gwbuf_free(b);
 		return;
 	}
 
-	memmove(b->buf, b->buf + len, b->len - len);
 	b->len -= len;
+	memmove(b->buf, &b->buf[len], b->len);
 	b->buf[b->len] = '\0';
 }
 
-int gwbuf_set_cap(struct gwbuf *b, size_t cap)
+__hot
+void gwbuf_soft_advance(struct gwbuf *b, uint64_t len)
 {
-	char *new_buf;
-
-	if (cap == b->cap)
-		return 0;
-
-	new_buf = realloc(b->buf, cap + 1);
-	if (!new_buf)
-		return -ENOMEM;
-
-	b->buf = new_buf;
-	b->cap = cap;
-	if (b->len > b->cap)
-		b->len = b->cap;
-
-	b->buf[b->len] = b->buf[b->cap] = '\0';
-	return 0;
+	assert(len <= b->len);
+	b->buf += len;
+	b->len -= len;
 }
 
+__hot
+void gwbuf_soft_advance_sync(struct gwbuf *b)
+{
+	assert(b->buf >= b->orig_buf);
+	assert(b->cap >= b->len);
+
+	if (!b->len) {
+		gwbuf_free(b);
+		return;
+	}
+
+	memmove(b->orig_buf, b->buf, b->len);
+	b->orig_buf[b->len] = '\0';
+	b->buf = b->orig_buf;
+}
+
+__hot
+int gwbuf_prepare_need(struct gwbuf *b, uint64_t need)
+{
+	uint64_t needed_len = b->len + need;
+	uint64_t new_cap;
+
+	if (need <= b->cap - b->len)
+		return 0;
+
+	new_cap = (b->cap + 1ull) * 2ull;
+	while (new_cap < needed_len)
+		new_cap *= 2ull;
+
+	return gwbuf_set_cap(b, new_cap);
+}
+
+__hot
 int gwbuf_apfmt(struct gwbuf *b, const char *fmt, ...)
 {
 	va_list args, args2;
+	uint64_t free_space;
 	int len, ret;
 
 	va_start(args, fmt);
@@ -95,40 +123,38 @@ int gwbuf_apfmt(struct gwbuf *b, const char *fmt, ...)
 	len = vsnprintf(NULL, 0, fmt, args2);
 	va_end(args2);
 
-	ret = gwbuf_increase(b, len + 1);
-	if (ret < 0)	
+	ret = gwbuf_prepare_need(b, len + 1);
+	if (ret < 0)
 		goto out;
 
-	ret = vsnprintf(b->buf + b->len, b->cap - b->len + 1, fmt, args);
+	free_space = b->cap - b->len;
+	ret = vsnprintf(&b->buf[b->len], free_space + 1ul, fmt, args);
 	b->len += ret;
-	b->buf[b->len] = '\0';
 	ret = 0;
 out:
 	va_end(args);
 	return ret;
 }
 
-int gwbuf_append(struct gwbuf *b, const void *data, size_t len)
+__hot
+int gwbuf_append(struct gwbuf *b, const void *data, uint64_t len)
 {
-	static const size_t base_buf = 1023;
+	int ret;
 
-	if (b->len + len > b->cap) {
-		size_t new_cap = b->cap ? b->cap * 2 : base_buf;
-		char *new_buf;
+	ret = gwbuf_prepare_need(b, len);
+	if (ret < 0)
+		return ret;
 
-		if (new_cap < b->len + len)
-			new_cap = b->len + len + base_buf;
-
-		new_buf = realloc(b->buf, new_cap + 1);
-		if (!new_buf)
-			return -ENOMEM;
-
-		b->buf = new_buf;
-		b->cap = new_cap;
-	}
-
-	memcpy(b->buf + b->len, data, len);
+	memcpy(&b->buf[b->len], data, len);
 	b->len += len;
-	b->buf[b->len] = b->buf[b->cap] = '\0';
+	b->buf[b->len] = '\0';
 	return 0;
+}
+
+__hot
+void gwbuf_move(struct gwbuf *dst, struct gwbuf *src)
+{
+	gwbuf_free(dst);
+	*dst = *src;
+	memset(src, 0, sizeof(*src));
 }

gwbuf.h
@@ -6,19 +6,24 @@
 #define GWBUF_H
 
 #include <stddef.h>
+#include <stdint.h>
 
 struct gwbuf {
-	char	*buf;
-	size_t	len;
-	size_t	cap;
+	char		*buf;
+	uint64_t	len;
+	uint64_t	cap;
+	char		*orig_buf;
 };
 
-int gwbuf_init(struct gwbuf *b, size_t cap);
-int gwbuf_increase(struct gwbuf *b, size_t inc);
+int gwbuf_init(struct gwbuf *b, uint64_t cap);
+int gwbuf_increase(struct gwbuf *b, uint64_t inc);
 void gwbuf_free(struct gwbuf *b);
-void gwbuf_advance(struct gwbuf *b, size_t len);
-int gwbuf_set_cap(struct gwbuf *b, size_t cap);
+void gwbuf_advance(struct gwbuf *b, uint64_t len);
+void gwbuf_soft_advance(struct gwbuf *b, uint64_t len);
+void gwbuf_soft_advance_sync(struct gwbuf *b);
+int gwbuf_prepare_need(struct gwbuf *b, uint64_t need);
 int gwbuf_apfmt(struct gwbuf *b, const char *fmt, ...);
-int gwbuf_append(struct gwbuf *b, const void *data, size_t len);
+int gwbuf_append(struct gwbuf *b, const void *data, uint64_t len);
+void gwbuf_move(struct gwbuf *dst, struct gwbuf *src);
 
 #endif /* #ifndef GWBUF_H */

gwhttpd.c
@@ -1,0 +1,320 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (C) 2025  Ammar Faizi <ammarfaizi2@gnuweeb.org>
+ */
+
+#define GWNET_HTTP_DEFINE_SHORT_NAMES
+#include "gwnet_http.h"
+#include "gwbuf.h"
+
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <getopt.h>
+
+#include <signal.h>
+
+static const struct option long_opts[] = {
+	{ "bind-addr",		required_argument,	NULL,	'b' },
+	{ "port",		required_argument,	NULL,	'p' },
+	{ "nr-workers",		required_argument,	NULL,	'n' },
+	{ "reuse-addr",		required_argument,	NULL,	'r' },
+	{ "reuse-port",		required_argument,	NULL,	'R' },
+	{ "tcp-backlog",	required_argument,	NULL,	't' },
+	{ "max-req-hdr-len",	required_argument,	NULL,	'H' },
+	{ "max-req-body-len",	required_argument,	NULL,	'B' },
+	{ "help",		no_argument,		NULL,	'h' },
+#ifdef GWNET_HTTP1_TESTS
+	{ "run-tests",		no_argument,		NULL,	1 },
+#endif
+	{ NULL, 0, NULL, 0 }
+};
+static const char short_opts[] = "b:p:n:r:R:t:H:B:h";
+
+static const struct gwnet_http_srv_cfg default_cfg = {
+	.tcp_cfg = {
+		.bind_addr = "::",
+		.port = 8080,
+		.nr_workers = 3,
+		.reuse_addr = 1,
+		.reuse_port = 1,
+		.tcp_backlog = 4096
+	},
+	.max_req_hdr_len = GWNET_HTTP_DEF_MAX_REQ_HDR_LEN,
+	.max_req_body_len = GWNET_HTTP_DEF_MAX_REQ_BODY_LEN
+};
+
+static gwnet_http_srv_t *g_srv = NULL;
+
+static void signal_handler(int sig)
+{
+	(void)sig;
+
+	if (g_srv)
+		gwnet_http_srv_stop(g_srv);
+}
+
+static void show_help(const char *app)
+{
+	const struct gwnet_tcp_srv_cfg *tc = &default_cfg.tcp_cfg;
+	const struct gwnet_http_srv_cfg *hc = &default_cfg;
+
+	printf("Usage: %s [OPTIONS]\n", app);
+	printf("gwhttpd2 - A simple HTTP server\n");
+	printf("\n");
+	printf("Options:\n");
+	printf("  -b, --bind-addr <addr>        Bind address (default: %s)\n", tc->bind_addr);
+	printf("  -p, --port <port>             Port number (default: %u)\n", tc->port);
+	printf("  -n, --nr-workers <num>        Number of worker threads (default: %u)\n", tc->nr_workers);
+	printf("  -r, --reuse-addr <0|1>        Enable/disable SO_REUSEADDR (default: %d)\n", tc->reuse_addr);
+	printf("  -R, --reuse-port <0|1>        Enable/disable SO_REUSEPORT (default: %d)\n", tc->reuse_port);
+	printf("  -t, --tcp-backlog <num>       TCP backlog size (default: %d)\n", tc->tcp_backlog);
+	printf("  -H, --max-req-hdr-len <len>   Maximum request header length (default: %u)\n", hc->max_req_hdr_len);
+	printf("  -B, --max-req-body-len <len>  Maximum request body length (default: %llu)\n", (unsigned long long)hc->max_req_body_len);
+	printf("  -h, --help                    Show this help message and exit\n");
+	printf("  --run-tests                   Run tests and exit\n");
+	printf("\n");
+}
+
+uint64_t atoull_kgmt(const char *str, int *err)
+{
+	char *endptr;
+	uint64_t value = strtoull(str, &endptr, 10);
+
+	if (endptr == str || *endptr == '\0') {
+		*err = 0;
+		return value;
+	}
+
+	if (*endptr == 'k' || *endptr == 'K') {
+		value *= 1024;
+		endptr++;
+	} else if (*endptr == 'm' || *endptr == 'M') {
+		value *= 1024 * 1024;
+		endptr++;
+	} else if (*endptr == 'g' || *endptr == 'G') {
+		value *= 1024 * 1024 * 1024;
+		endptr++;
+	}
+
+	if (*endptr != '\0') {
+		*err = -EINVAL;
+		return 0;
+	}
+
+	*err = 0;
+	return value;
+}
+
+static int __parse_arg(const char *app, int c, struct gwnet_tcp_srv_cfg *tc,
+		       struct gwnet_http_srv_cfg *hc)
+{
+	int tmp;
+
+	switch (c) {
+	case 'b':
+		strncpy(tc->bind_addr, optarg, sizeof(tc->bind_addr));
+		tc->bind_addr[sizeof(tc->bind_addr) - 1] = '\0';
+		break;
+	case 'p':
+		tmp = atoi(optarg);
+		if (tmp < 0 || tmp > 65535) {
+			fprintf(stderr, "Error: Invalid port number: %s\n", optarg);
+			return -EINVAL;
+		}
+		tc->port = (uint16_t)tmp;
+		break;
+	case 'n':
+		tmp = atoi(optarg);
+		if (tmp < 1 || tmp > 1024) {
+			fprintf(stderr, "Error: Invalid number of workers: %s\n", optarg);
+			return -EINVAL;
+		}
+		tc->nr_workers = (uint16_t)tmp;
+		break;
+	case 'r':
+		if (!strcmp(optarg, "1") || !strcasecmp(optarg, "true")) {
+			tc->reuse_addr = 1;
+		} else if (!strcmp(optarg, "0") || !strcasecmp(optarg, "false")) {
+			tc->reuse_addr = 0;
+		} else {
+			fprintf(stderr, "Error: Invalid reuse-addr value: %s\n", optarg);
+			return -EINVAL;
+		}
+		break;
+	case 'R':
+		if (!strcmp(optarg, "1") || !strcasecmp(optarg, "true")) {
+			tc->reuse_port = 1;
+		} else if (!strcmp(optarg, "0") || !strcasecmp(optarg, "false")) {
+			tc->reuse_port = 0;
+		} else {
+			fprintf(stderr, "Error: Invalid reuse-port value: %s\n", optarg);
+			return -EINVAL;
+		}
+		break;
+	case 't':
+		tmp = atoi(optarg);
+		if (tmp < 0 || tmp > 1024) {
+			fprintf(stderr, "Error: Invalid TCP backlog: %s\n", optarg);
+			return -EINVAL;
+		}
+		tc->tcp_backlog = tmp;
+		break;
+	case 'H':
+		tmp = atoi(optarg);
+		if (tmp < 0 || tmp > 65536) {
+			fprintf(stderr, "Error: Invalid max request header length: %s\n", optarg);
+			return -EINVAL;
+		}
+		hc->max_req_hdr_len = (uint32_t)tmp;
+		break;
+	case 'B':
+		hc->max_req_body_len = atoull_kgmt(optarg, &tmp);
+		if (tmp < 0) {
+			fprintf(stderr, "Error: Invalid max request body length: %s\n", optarg);
+			return -EINVAL;
+		}
+		break;
+	case 'h':
+		show_help(app);
+		exit(0);
+#ifdef GWNET_HTTP1_TESTS
+	case 1:
+		gwnet_http_run_tests();
+		exit(0);
+		break;
+#endif
+	default:
+		fprintf(stderr, "Error: Unknown option: %c\n", c);
+		show_help(app);
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
+static int parse_arg_and_init(int argc, char *argv[], gwnet_http_srv_t **srv_p)
+{
+	const struct sigaction sa = {
+		.sa_handler = &signal_handler,
+		.sa_flags = 0
+	};
+	struct gwnet_http_srv_cfg cfg = default_cfg;
+	struct gwnet_tcp_srv_cfg *tc = &cfg.tcp_cfg;
+	struct gwnet_http_srv_cfg *hc = &cfg;
+	gwnet_http_srv_t *srv;
+	int ret;
+
+	while (1) {
+		int c = getopt_long(argc, argv, short_opts, long_opts, NULL);
+		if (c == -1)
+			break;
+
+		ret = __parse_arg(argv[0], c, tc, hc);
+		if (ret < 0)
+			return ret;
+	}
+
+	ret = 0;
+	ret |= sigaction(SIGINT, &sa, NULL);
+	ret |= sigaction(SIGTERM, &sa, NULL);
+	if (ret) {
+		ret = -errno;
+		fprintf(stderr, "Error: Failed to set signal handlers: %s\n",
+			strerror(-ret));
+		return ret;
+	}
+
+	srv = gwnet_http_srv_init(&cfg);
+	if (!srv) {
+		fprintf(stderr, "Error: Failed to initialize HTTP server\n");
+		return -ENOMEM;
+	}
+
+	*srv_p = g_srv = srv;
+	return 0;
+}
+
+#if 0
+struct rt_ctx {
+	struct gwnet_http_srv *srv;
+	struct gwnet_http_cli *hc;
+	struct gwnet_http_req *req;
+	struct gwnet_http_res *res;
+};
+
+static int route_hello_world(struct rt_ctx *ctx)
+{
+	struct gwnet_http_res *res = ctx->res;
+	struct gwbuf *b = hres_get_body_buf(res);
+
+	hres_set_content_type(res, "text/plain");
+	gwnet_http_res_set_type(res, GWNET_HTTP_RES_TYPE_BUF);
+	gwbuf_apfmt(b, "Hello world!\n");
+	hres_set_code(res, 200);
+	return 0;
+}
+
+static int route_zero(struct rt_ctx *ctx)
+{
+	struct gwnet_http_res *res = hres_get(ctx->hc);
+
+	hres_set_content_type(res, "application/octet-stream");
+	gwnet_http_res_set_type(res, GWNET_HTTP_RES_TYPE_ZERO);
+	hres_set_zero_len(res, 1024ull * 1024 * 1024 * 30);
+	hres_set_code(res, 200);
+	return 0;
+}
+
+static int route_404(struct rt_ctx *ctx)
+{
+	struct gwnet_http_res *res = ctx->res;
+
+	hres_set_content_type(res, "text/plain");
+	gwnet_http_res_set_type(res, GWNET_HTTP_RES_TYPE_BUF);
+	gwbuf_apfmt(hres_get_body_buf(res), "404 Not Found\n");
+	hres_set_code(res, 404);
+	return 0;
+}
+
+static int handle_route(struct gwnet_http_srv *srv, struct gwnet_http_cli *hc)
+{
+	struct gwnet_http_req *req = hreq_get(hc);
+	struct gwnet_http_res *res = hres_get(hc);
+	struct rt_ctx ctx = {
+		.srv = srv,
+		.hc = hc,
+		.req = req,
+		.res = res,
+	};
+	char *uri = hreq_get_nc_uri(req);
+
+	if (!uri)
+		return -EINVAL;
+
+	uri = trim_char(uri, '/');
+	if (!*uri)
+		return route_hello_world(&ctx);
+
+	if (!strcmp(uri, "zero"))
+		return route_zero(&ctx);
+
+	return route_404(&ctx);
+}
+#endif
+
+int main(int argc, char *argv[])
+{
+	gwnet_http_srv_t *srv;
+	int ret;
+
+	ret = parse_arg_and_init(argc, argv, &srv);
+	if (ret)
+		return -ret;
+
+	ret = gwnet_http_srv_run(srv);
+	gwnet_http_srv_free(srv);
+	return -ret;
+}

commit 5dd45dcb4275f61afa4ef7aab133f5c696c3bb58

gwnet_http: Fix set HTTP code function
The parameter must be at least uint16_t as the HTTP code can
be more than 255.

Fixes: c3fd714c0525227a06c1e5b62a4a1be9fd3d295c ("Full project rewrite")
Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

gwnet_http.c
@@ -234,7 +234,7 @@ struct gwnet_http_res *gwnet_http_req_get_res(gwnet_http_req_t *req)
 	return &req->res;
 }
 
-void gwnet_http_res_set_code(gwnet_http_res_t *res, uint8_t code)
+void gwnet_http_res_set_code(gwnet_http_res_t *res, uint16_t code)
 {
 	res->status = code;
 }

gwnet_http.h
@@ -75,7 +75,7 @@ void gwnet_http_cli_set_rt_on_body_cb(gwnet_http_cli_t *hc,
 
 struct gwnet_http_res *gwnet_http_req_get_res(gwnet_http_req_t *req);
 
-void gwnet_http_res_set_code(gwnet_http_res_t *res, uint8_t code);
+void gwnet_http_res_set_code(gwnet_http_res_t *res, uint16_t code);
 void gwnet_http_res_set_content_type(gwnet_http_res_t *res,
 				     const char *content_type);
 void gwnet_http_res_body_set_zero(gwnet_http_res_t *res, uint64_t len);