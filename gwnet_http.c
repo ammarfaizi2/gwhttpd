ammarfaizi2/gwhttpd

commit ea90e77441bdda55f987572b545e5ba95edbe206

gwnet_http: Don't pass incomplete request to `handle_tx()`
Make sure to only perform response processing on fully received
requests to avoid stuck in `GWNET_HTTP_TX_ST_HDR` state.

Fixes: c3fd714c0525 ("Full project rewrite")
Reported-by: Ahmad Gani <reyuki@gnuweeb.org>
Link: https://t.me/GNUWeeb/1174635
Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

gwnet_http.c
@@ -88,6 +88,7 @@ struct gwnet_http_req {
 	bool				is_chunked;
 	bool				is_body_oversized;
 	bool				keep_alive;
+	bool				is_res_ready;
 	union {
 		struct gwnet_http_hdr_pctx	hpctx;
 		struct gwnet_http_body_pctx	bpctx;
@@ -794,6 +795,7 @@ static int handle_rx_st_done(gwnet_http_cli_t *hc)
 	if (!req->keep_alive)
 		hc->stop_receiving = true;
 
+	req->is_res_ready = true;
 	return r;
 }
 
@@ -1105,11 +1107,15 @@ static int handle_tx(struct gwnet_http_cli *hc, gwnet_tcp_cli_t *c)
 __hot
 static int handle_req_done(struct gwnet_http_cli *hc, gwnet_tcp_cli_t *c)
 {
-	hc->rx_state = GWNET_HTTP_RX_ST_INIT;
-	if (hc->tx_state != GWNET_HTTP_TX_ST_INIT)
-		return 0;
+	struct gwnet_http_req *req = gwnet_http_srv_cli_req_head(hc);
+	int r = 0;
 
-	return handle_tx(hc, c);
+	assert(req);
+	hc->rx_state = GWNET_HTTP_RX_ST_INIT;
+	if (req && req->is_res_ready)
+		r = handle_tx(hc, c);
+
+	return r;
 }
 
 __hot

commit 586b1544a928bedd91cb059b530336799feae3b1

gwnet_http: Fix missing `is_res_ready` check on multipe `__handle_tx()` call stacks
Fix missing `is_res_ready` check on multipe `__handle_tx()`
call stacks by moving `is_res_ready` check to `__handle_tx()`
to cover all calling paths.

Fixes: ea90e77441bdda55f987572b545e5ba95edbe206 ("gwnet_http: Don't pass incomplete request to `handle_tx()`")
Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

gwnet_http.c
@@ -1060,6 +1060,9 @@ static int __handle_tx(struct gwnet_http_cli *hc, struct gwnet_http_req *req,
 {
 	int r = 0;
 
+	if (!req->is_res_ready)
+		return r;
+
 	switch (hc->tx_state) {
 	case GWNET_HTTP_TX_ST_INIT:
 		r = handle_tx_init(hc, req);
@@ -1107,15 +1110,8 @@ static int handle_tx(struct gwnet_http_cli *hc, gwnet_tcp_cli_t *c)
 __hot
 static int handle_req_done(struct gwnet_http_cli *hc, gwnet_tcp_cli_t *c)
 {
-	struct gwnet_http_req *req = gwnet_http_srv_cli_req_head(hc);
-	int r = 0;
-
-	assert(req);
 	hc->rx_state = GWNET_HTTP_RX_ST_INIT;
-	if (req && req->is_res_ready)
-		r = handle_tx(hc, c);
-
-	return r;
+	return handle_tx(hc, c);
 }
 
 __hot